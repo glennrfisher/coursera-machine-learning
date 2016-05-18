4980eb930b92614fd9132d3e43e912e646cea014
Glenn R. Fisher <glennrfisher@gmail.com>
2016-05-18 18:59:19 -0500

    Add solutions to assignment 4

--- ex4/ex4/nnCostFunction.m
+++ ex4/ex4/nnCostFunction.m
@@ -62,25 +62,43 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Transform y to a one-hot encoding
+y_encoded = zeros(m, num_labels);
+for i = 1:m
+	y_encoded(i, y(i)) = 1;
+end
+y = y_encoded;
 
+% Feedforward to compute predictions
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(m, 1) sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+% Compute unregularized cost function
+J = sum(sum((-y) .* log(a3) - (1-y) .* log(1-a3))) / m;
 
+% Compute regularized cost function
+reg_theta1 = sum(sum(Theta1(:, 2:end) .^ 2));
+reg_theta2 = sum(sum(Theta2(:, 2:end) .^ 2));
+J = J + lambda / (2*m) * (reg_theta1 + reg_theta2);
 
+% calculate deltas for each unit
+delta3 = a3 - y;
+delta2 = delta3 * Theta2(:, 2:end) .* sigmoidGradient(z2);
 
+% calculate deltas for each parameter
+Delta2 = delta3' * a2;
+Delta1 = delta2' * a1;
 
+% calculate gradient for each parameter
+Theta1_grad = Delta1 / m;
+Theta2_grad = Delta2 / m;
 
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
+% Compute regularized gradient for each parameter
+Theta1_grad = [Theta1_grad(:, 1), Theta1_grad(:, 2:end) + (lambda/m) * Theta1(:, 2:end)];
+Theta2_grad = [Theta2_grad(:, 1), Theta2_grad(:, 2:end) + (lambda/m) * Theta2(:, 2:end)];
 
 % =========================================================================
 
